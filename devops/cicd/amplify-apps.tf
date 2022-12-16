6f70d6c6cc6d1c96e7373c13135e87b7af27bc3e
Marek Majkut <marek.majkut@takeda.com>
2022-12-16 18:20:39 +0100

    solving amplify build issue

--- devops/cicd/amplify-apps.tf
+++ devops/cicd/amplify-apps.tf
@@ -28,12 +28,25 @@ resource "aws_amplify_app" "my-amplify-app" {
         appRoot: ${var.REPOSITORY_FOLDER_PATH}
   EOT
 
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
+  }
+  custom_rule {
+    source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json)$)([^.]+$)/>"
+    status = "200"
+    target = "/index.html"
+  }
+
   environment_variables = {
-    "AMPLIFY_DIFF_DEPLOY": "true",
+    "AMPLIFY_DIFF_DEPLOY": "false",
     "AMPLIFY_MONOREPO_APP_ROOT": "frontend"
   }
 }
 resource "aws_amplify_branch" "my-amplify-branch" {
   branch_name = var.git-branch-name
   app_id = aws_amplify_app.my-amplify-app.id
+  framework = "Angular"
+  stage = "PRODUCTION"
 }
