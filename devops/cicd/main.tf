292738a9a9f5eda9a9716b343a7a910120373fbc
Marek Majkut <marek.majkut@takeda.com>
2023-01-02 16:08:01 +0100

    VCS workspace setup

--- devops/cicd/main.tf
+++ devops/cicd/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  cloud {
-    organization = "marekmajkut"
-    workspaces {
-      name = "proj-tf-hcl"
-    }
-  }
+  # cloud {
+  #   organization = "marekmajkut"
+  #   workspaces {
+  #     name = "proj-tf-hcl"
+  #   }
+  # }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -14,4 +14,19 @@ terraform {
 }
 provider "aws" {
   region = "us-east-1"
+  shared_credentials_files = ["C:/Users/fvg3843/.aws/credentials"]
+  profile = "private"
+  tags = {
+    apms-id = "example"
+    application-name = "example"
+    application-owner = "example"
+    asec-f = "example"
+    business-unit-n1 = "example"
+    business-unit-n2 = "example"
+    environment-id = "example"
+    it-technical-owner = "example"
+    version = "example"
+    responsible-person = "example"
+    deployment-option = "example"
+  }
 }
