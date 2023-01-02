1fb9c1597f1ba430907d278629946a9fa64ea1d3
Marek Majkut <marek.majkut@takeda.com>
2023-01-02 16:10:46 +0100

    tags fix

--- devops/cicd/main.tf
+++ devops/cicd/main.tf
@@ -16,17 +16,19 @@ provider "aws" {
   region = "us-east-1"
   shared_credentials_files = ["C:/Users/fvg3843/.aws/credentials"]
   profile = "private"
-  tags = {
-    apms-id = "example"
-    application-name = "example"
-    application-owner = "example"
-    asec-f = "example"
-    business-unit-n1 = "example"
-    business-unit-n2 = "example"
-    environment-id = "example"
-    it-technical-owner = "example"
-    version = "example"
-    responsible-person = "example"
-    deployment-option = "example"
+  default_tags {
+    tags = {
+      apms-id = "example"
+      application-name = "example"
+      application-owner = "example"
+      asec-f = "example"
+      business-unit-n1 = "example"
+      business-unit-n2 = "example"
+      environment-id = "example"
+      it-technical-owner = "example"
+      version = "example"
+      responsible-person = "example"
+      deployment-option = "example"
+    }
   }
 }
