efc04fde1926f16604f61b91cb08fc712dca25dc
Marek Majkut <marek.majkut@takeda.com>
2023-01-03 15:13:44 +0100

    lambda function first go

---
+++ devops/cicd/lambda-functions.tf
@@ -0,0 +1,11 @@
+module "my-lambda-function-id" {
+  source = "./modules/resource-id"
+  branch-name = var.GIT_BRANCH_NAME
+  resource-name = "my-lambda-function"
+}
+resource "aws_lambda_function" "my-lambda-function" {
+  function_name = module.my-lambda-function-id.resource-id
+  filename = "C:/Users/fvg3843/PROJECTS/PROJ/proj-tf-hcl/backend/integration/my-integration-lambda/package.zip"
+  handler = "dist/app.lambdaHandler"
+  runtime = "nodejs18.x"
+}
