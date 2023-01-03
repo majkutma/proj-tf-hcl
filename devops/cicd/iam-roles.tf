da4d0100ba6b9edc59a00225e8fafc4415cfdd0a
Marek Majkut <marek.majkut@takeda.com>
2023-01-03 15:21:43 +0100

    lambda function another go

---
+++ devops/cicd/iam-roles.tf
@@ -0,0 +1,23 @@
+module "my-iam-role-id" {
+  source = "./modules/resource-id"
+  branch-name = var.GIT_BRANCH_NAME
+  resource-name = "my-iam-role"
+}
+resource "aws_iam_role" "my-iam-role" {
+  name = module.my-iam-role-id.resource-id
+  assume_role_policy = <<EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "lambda.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    }
+  EOF
+}
