0d68253447e420bef63fffd08d1845097489a869
Marek Majkut <marek.majkut@takeda.com>
2023-01-03 16:10:31 +0100

    updating

--- devops/cicd/lambda-functions.tf
+++ devops/cicd/lambda-functions.tf
@@ -1,18 +1,18 @@
-# module "my-lambda-function-id" {
-#   source = "./modules/resource-id"
-#   branch-name = var.GIT_BRANCH_NAME
-#   resource-name = "my-lambda-function"
-# }
-# data "archive_file" "zipit" {
-#   type        = "zip"
-#   source_dir = "../../devops/cicd/lambda-dist/dist"
-#   # source_dir = "../../backend/integration/my-integration-lambda/dist"
-#   output_path = "tf_lambda.zip"
-# }
-# resource "aws_lambda_function" "my-lambda-function" {
-#   function_name = module.my-lambda-function-id.resource-id
-#   role = aws_iam_role.my-iam-role.arn
-#   filename = "tf_lambda.zip"
-#   handler = "dist/app.lambdaHandler"
-#   runtime = "nodejs18.x"
-# }
+module "my-lambda-function-id" {
+  source = "./modules/resource-id"
+  branch-name = var.GIT_BRANCH_NAME
+  resource-name = "my-lambda-function"
+}
+data "archive_file" "zipit" {
+  type        = "zip"
+  source_dir = "../../devops/cicd/lambda-dist/dist"
+  # source_dir = "../../backend/integration/my-integration-lambda/dist"
+  output_path = "tf_lambda.zip"
+}
+resource "aws_lambda_function" "my-lambda-function" {
+  function_name = module.my-lambda-function-id.resource-id
+  role = aws_iam_role.my-iam-role.arn
+  filename = "tf_lambda.zip"
+  handler = "dist/app.lambdaHandler"
+  runtime = "nodejs18.x"
+}
