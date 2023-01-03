bddeb206988685aa3dc195fd9188b1a174cd3996
Marek Majkut <marek.majkut@takeda.com>
2023-01-03 10:38:59 +0100

    white spaces and resource-id

--- devops/cicd/modules/resource-id/main.tf
+++ devops/cicd/modules/resource-id/main.tf
@@ -1,8 +1,13 @@
 locals {
   resource-id = substr(
-    replace(
-      join("-", [terraform.workspace, var.branch-name, var.resource-name]), "--", "-"
+    lower(
+      replace(
+        replace(
+          join(
+            "-", [terraform.workspace, var.branch-name, var.resource-name]
+          ), "/[^a-zA-Z0-9-]/", "-"
+        ),"/-{2,}/","-"
+      )
     ), 0, 62
   )
 }
-
