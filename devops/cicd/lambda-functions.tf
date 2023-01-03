255124f2b2982b8c5f4f2bdc0547e45aaf0e950a
Marek Majkut <marek.majkut@takeda.com>
2023-01-03 15:57:45 +0100

    changing paths

--- devops/cicd/lambda-functions.tf
+++ devops/cicd/lambda-functions.tf
@@ -5,7 +5,7 @@ module "my-lambda-function-id" {
 }
 data "archive_file" "zipit" {
   type        = "zip"
-  source_file = "lambda-dist/dist/app.js"
+  source_dir = "lambda-dist/dist"
   output_path = "tf_lambda.zip"
 }
 resource "aws_lambda_function" "my-lambda-function" {
