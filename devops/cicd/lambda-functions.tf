d9f48e21ef33401e1fc4ca7e12155df6b2942d95
Marek Majkut <marek.majkut@takeda.com>
2023-01-03 15:37:57 +0100

    using file archive

--- devops/cicd/lambda-functions.tf
+++ devops/cicd/lambda-functions.tf
@@ -3,11 +3,15 @@ module "my-lambda-function-id" {
   branch-name = var.GIT_BRANCH_NAME
   resource-name = "my-lambda-function"
 }
+data "archive_file" "zipit" {
+  type        = "zip"
+  source_file = "C:/Users/fvg3843/PROJECTS/PROJ/proj-tf-hcl/backend/integration/my-integration-lambda/dist"
+  output_path = "tf_lambda.zip"
+}
 resource "aws_lambda_function" "my-lambda-function" {
   function_name = module.my-lambda-function-id.resource-id
   role = aws_iam_role.my-iam-role.arn
-  # filename = "C:/Users/fvg3843/PROJECTS/PROJ/proj-tf-hcl/backend/integration/my-integration-lambda/package.zip"
-  filename = "C:\\Users\\fvg3843\\PROJECTS\\PROJ\\proj-tf-hcl\\backend\\integration\\my-integration-lambda\\package.zip"
+  filename = "tf_lambda.zip"
   handler = "dist/app.lambdaHandler"
   runtime = "nodejs18.x"
 }
