8e5918fc9af643473913a3a7161c28fae089a7bf
Marek Majkut <marek.majkut@takeda.com>
2023-01-05 15:47:37 +0100

    layer games

--- devops/cicd/lambda-layers.tf
+++ devops/cicd/lambda-layers.tf
@@ -3,13 +3,13 @@ module "my_lambda_layer_id" {
   branch_name = var.GIT_BRANCH_NAME
   resource_name = "my-lambda-layer"
 }
-data "archive_file" "zip-layer" {
-  type = "zip"
-  source_dir = "../lambda-dist/my-lambda-layer"
-  output_path = "ly-lambda-layer.zip"
-}
+# data "archive_file" "zip-layer" {
+#   type = "zip"
+#   source_dir = "../lambda-dist/my-lambda-layer"
+#   output_path = "my-lambda-layer.zip"
+# }
 resource "aws_lambda_layer_version" "my_lambda_layer" {
   layer_name = module.my_lambda_layer_id.resource_id
-  filename   = "lambda_nodejs_layer_payload.zip"
+  filename   = "../lambda-dist/node-modules.zip"
   compatible_runtimes = ["nodejs18.x"]
 }
