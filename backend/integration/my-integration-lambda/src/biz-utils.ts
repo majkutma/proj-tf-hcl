8375eb8cce37ede8c9ef32409a26300bee8f1126
Marek Majkut <marek.majkut@takeda.com>
2023-01-05 15:17:05 +0100

    adding node dependencies

--- backend/integration/my-integration-lambda/src/biz-utils.ts
+++ backend/integration/my-integration-lambda/src/biz-utils.ts
@@ -1,7 +1,10 @@
+import _ from 'lodash'
+
 /**
  * Function creates simple message based on the parameter
  */
 export const createMessage = (param: string): string => {
-  const message = `This is my parameter: ${param}`
+  const parmUpper: string = _.upperCase(param)
+  const message = `This is my parameter in uppercase: ${parmUpper}`
   return message
 }
--- devops/lambda-dist/my-integration-lambda/biz-utils.js
+++ devops/lambda-dist/my-integration-lambda/biz-utils.js
@@ -1,11 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createMessage = void 0;
+const lodash_1 = __importDefault(require("lodash"));
 /**
  * Function creates simple message based on the parameter
  */
 const createMessage = (param) => {
-    const message = `This is my parameter: ${param}`;
+    const parmUpper = lodash_1.default.upperCase(param);
+    const message = `This is my parameter in uppercase: ${parmUpper}`;
     return message;
 };
 exports.createMessage = createMessage;
