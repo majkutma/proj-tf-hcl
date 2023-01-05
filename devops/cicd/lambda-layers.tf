33a65c1708e20fd5ad3acc14b96032806e6d8d62
Marek Majkut <marek.majkut@takeda.com>
2023-01-05 18:05:58 +0100

    neviem uz co robit

--- devops/cicd/lambda-layers.tf
+++ devops/cicd/lambda-layers.tf
@@ -17,7 +17,7 @@ resource "null_resource" "lambda_dependencies" {
       cd ../lambda-dist/my-lambda-layer &&\
       mkdir ./node_install &&\
       cd ./node_install &&\
-      curl https://nodejs.org/dist/v12.16.1/node-v12.16.1.tar.gz | tar xz --strip-components=1 &&\
+      curl http://npmjs.org/install.sh | sh &&\
       export PATH="$PWD/bin:$PATH" &&\
       cd .. &&\
       npm install --production
