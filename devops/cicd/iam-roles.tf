30329ef033880b51617e21d7b439cd718b7192ae
Marek Majkut <marek.majkut@takeda.com>
2023-01-05 11:32:46 +0100

    testing basic execution role attachment

--- devops/cicd/iam-roles.tf
+++ devops/cicd/iam-roles.tf
@@ -3,10 +3,21 @@ module "my-iam-role-id" {
   branch-name = var.GIT_BRANCH_NAME
   resource-name = "my-iam-role"
 }
-data "aws_iam_policy" "aws-lambda-basic-execution-role" {
-  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+data "aws_iam_policy_document" "aws-lambda-trust-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
 }
 resource "aws_iam_role" "my-iam-role" {
   name = module.my-iam-role-id.resource-id
-  assume_role_policy = data.aws_iam_policy.aws-lambda-basic-execution-role.policy
+  assume_role_policy = data.aws_iam_policy_document.aws-lambda-trust-policy.policy
+}
+resource "aws_iam_role_policy_attachment" "my-attachment" {
+  role       = "${aws_iam_role.terraform_function_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
