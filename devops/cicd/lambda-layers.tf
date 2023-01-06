15c8f5509f5e8a40cf9abd8854650d941c4f5818
Marek Majkut <marek.majkut@takeda.com>
2023-01-06 11:27:28 +0100

    hopingggg

--- devops/cicd/lambda-layers.tf
+++ devops/cicd/lambda-layers.tf
@@ -17,7 +17,7 @@ resource "null_resource" "lambda_dependencies" {
       cd ../lambda-dist/my-lambda-layer &&\
       mkdir ./node_install &&\
       cd ./node_install &&\
-      curl https://nodejs.org/dist/v12.16.1/node-v12.16.1-linux-x64.tar.gz | tar xz --strip-components=1 &&\
+      curl https://nodejs.org/dist/v18.12.1/node-v18.12.1.tar.gz | tar xz --strip-components=1 &&\
       export PATH="$PWD/bin:$PATH" &&\
       cd .. &&\
       npm install
